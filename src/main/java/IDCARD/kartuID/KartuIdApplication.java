4b7e3d06ea491320179f0e654ec4a6d8684589cd
Alekuyz <axelagia@gmail.com>
2022-11-15 21:03:04 +0700

    COMMIT PERTAMA

---
+++ src/main/java/IDCARD/kartuID/KartuIdApplication.java
@@ -0,0 +1,13 @@
+package IDCARD.kartuID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartuIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KartuIdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IDCARD/kartuID/KartuIdApplicationTests.java
@@ -0,0 +1,13 @@
+package IDCARD.kartuID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartuIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
